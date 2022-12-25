8820cde8de84f29d1a905757b37d1df7968b98ab
m4rcer <m4rcer@gmail.com>
2022-12-25 16:30:31 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace lab_7_g
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.drawBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1614, 926);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(1229, 835);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(164, 65);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Очистить";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(1424, 835);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(164, 65);
+            this.drawBtn.TabIndex = 2;
+            this.drawBtn.Text = "Нарисовать";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.drawBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1614, 926);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button drawBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_7_g
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pictureBoxBitMap;
+        Bitmap spriteBitMap;
+        Bitmap cloneBitMap;
+        Graphics g_pictureBox;
+        Graphics g_sprite;
+        int x, y;
+        int w, h;
+        Timer timer;
+        string basePath;
+        Image sky;
+        Image sea;
+        Image sheep;
+
+        public Form1()
+        {
+            InitializeComponent();
+            basePath = Directory.GetCurrentDirectory();
+            sky = Image.FromFile($"{basePath}/img/sky.jpg");
+            sea = Image.FromFile($"{basePath}/img/sea.png");
+            sheep = Image.FromFile($"{basePath}/img/sheep.png");
+            this.DoubleBuffered = true;
+        }
+
+        private void DrawSprite()
+        {
+            var rocket = Image.FromFile($"{basePath}/img/rocket.png");
+            g_sprite.DrawImage(rocket, 0, 0, w, h);
+        }
+
+        void SavePart(int xt, int yt)
+        {
+            try
+            {
+                Rectangle cloneRect = new Rectangle(xt, yt, w, h);
+                System.Drawing.Imaging.PixelFormat format =
+                pictureBoxBitMap.PixelFormat;
+                cloneBitMap = pictureBoxBitMap.Clone(cloneRect, format);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        void DrawBackgroud()
+        {
+            var g = Graphics.FromImage(pictureBoxBitMap);
+            g.DrawImage(sky, new Rectangle(0, 0, 1366, 720));
+            g.DrawImage(sheep, new Rectangle(150, 50, 900, 550));
+            g.DrawImage(sea, new Rectangle(0, 0, 1366, 720));
+        }
+
+        private void drawBtn_Click(object sender, EventArgs e)
+        {
+            w = 30;
+            h = 85;
+
+            x = 665;
+            y = 185;
+
+            g_pictureBox = pictureBox.CreateGraphics();
+            pictureBoxBitMap = new Bitmap(1366, 720);
+
+            DrawBackgroud();
+
+            pictureBox.Image = pictureBoxBitMap;
+
+            spriteBitMap = new Bitmap(w, h);
+            g_sprite = Graphics.FromImage(spriteBitMap);
+
+            DrawSprite();
+
+            cloneBitMap = new Bitmap(w, h);
+            SavePart(x, y);
+
+            g_pictureBox.DrawImage(spriteBitMap, x, y);
+
+            pictureBox.Invalidate();
+
+
+            timer = new Timer();
+            timer.Interval = 100;            timer.Tick += new EventHandler(timer_Tick);
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (y > pictureBox.Height)
+            {
+                timer.Stop();
+                return;
+            }
+
+            g_pictureBox.DrawImage(cloneBitMap, x, y);
+            y -= 5;
+
+            SavePart(x, y);
+            g_pictureBox.DrawImage(spriteBitMap, x, y);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            var g = pictureBox.CreateGraphics();
+            g.Clear(pictureBox.BackColor);
+            g.Dispose();
+            timer.Tick -= new EventHandler(timer_Tick);
+        }
+    }
+}
